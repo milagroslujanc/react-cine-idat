ce660ab826d9945a9dfb56aa6c7a59750917d9d6
Jhefferson <jhefferson.albornoz@gmail.com>
2025-07-21 22:48:42 -0500

    fetch de datos

--- src/components/list_movies.jsx
+++ src/components/list_movies.jsx
@@ -1,30 +1,40 @@
+import { useEffect, useState } from 'react'
 import '../styles/Body.css'
 
-const MOVIES = [ // mock de los datos
-    {id: 1,path: '/img/image 1.png',},
-    {id: 2,path: '/img/image 2.png',},
-    {id: 3,path: '/img/image 3.png',},
-    {id: 4,path: '/img/image 4.png',},
-    {id: 6,path: '/img/image 2.png',},
-    {id: 5,path: '/img/image 1.png',},
-    {id: 8,path: '/img/image 4.png',},
-    {id: 7,path: '/img/image 3.png',},
-    {id: 9,path: '/img/image 2.png',},
-    {id: 11,path: '/img/image 4.png',},
-    {id: 12,path: '/img/image 1.png',},
-    {id: 10,path: '/img/image 3.png',},
-]
-
 export function ListMovies() {
+
+    // consulta a la api de obtener las películas
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const fetchMovies = async () => {
+            const api = "https://backend-cine-idat-production.up.railway.app"
+            try {
+                const response = await fetch(`${api}/cines/cartelera`)
+                console.log(response)
+                if (!response.ok) {
+                    throw new Error('Network response was not ok')
+                }
+                const data = await response.json()
+                setMovies(data)
+            } catch (error) {
+                console.error('Error fetching movies:', error)
+            }
+        }
+
+        fetchMovies()
+    }, [])
+
     return (
         <div className="container list-movies">
             <h2 className='text-color'>Películas</h2>
             <span>Mira toda nuestra cartelera</span>
             <div className='list-movies-container'>
-                {
-                    MOVIES.map(movie => (
-                        <img src={movie.path} key={movie.id}/>
-                    ) )
+                {movies.length === 0 ?
+                    <p>Cargando películas...</p> :
+                    movies.data.map(movie => (
+                        <img src={movie.imgUrl} key={movie.id} />
+                    ))
                 }
             </div>
         </div>
